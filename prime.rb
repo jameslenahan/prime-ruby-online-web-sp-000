8032838b2bd7dcd6a9e267a4ab78e18fc7ec8af9
James Lenahan <james.k.lenahan@gmail.com>
2019-12-02 15:25:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
-def prime?(n)
-  if n %2 != 0 && n != 2 
-    return true
+def prime(n)
+  unless n.is_a? Integer
+  puts "That's not an integer."
+  if n%2 !=0 && n!=2
+    puts "#{n} is prime!"
   else
-    return false
+    puts "#{n} is not prime."
   end
 end
 
-prime?(2)
+prime(2)
